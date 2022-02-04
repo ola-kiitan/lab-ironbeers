d2ee992573ba61f91b864f278bbdde3dc5f03044
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-02-04 07:47:46 +0100

    WDFT Ber/Jan 22

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+const links = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +20,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    doctitle: 'home-page',
+    links: [
+      { href: '/beers', linkText: 'beers' },
+      { href: '/random-beer', linkText: 'random-beer' }
+    ]
+  });
 });
-
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi =>
+      res.render('beers', {
+        doctitle: 'beers-page',
+        beers: beersFromApi,
+        links: [
+          { href: '/', linkText: 'home-page' },
+          { href: '/random-beer', linkText: 'random-beer' }
+        ]
+      })
+    )
+    .catch(error => console.log(error));
+});
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beer', {
+        doctitle: 'random-beer',
+        randomBeer: responseFromAPI,
+        links: [
+          { href: '/', linkText: 'home-page' },
+          { href: '/beers', linkText: 'beers' }
+        ]
+      });
+      console.log(responseFromAPI);
+    })
+    .catch(error => console.log(error));
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
